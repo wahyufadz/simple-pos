85f577b408dec571c66675225459c376dc0334d0
x <wahyufadz@gmail.com>
2019-02-22 18:14:26 +0800

    hot reloading with browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,12 @@ const mix = require('laravel-mix');
 
 mix.react('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
+
+   //disable mixin notification after compiling js
+   .disableNotifications()
+
+    //hot reloading server
+    .browserSync({
+        proxy:'localhost:8000',
+        notify:false
+    })
